709db9c7f5b747b3f42f32ecd60cd897e75544d9
Grace Choi <oiograceoio@gmail.com>
2019-03-20 23:49:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,14 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      SQL
+    DB[:conn].execute(sql, x)
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
