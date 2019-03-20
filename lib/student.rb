f4e454a3c6c319a456fcfd497c60a5cc10a9e580
Grace Choi <oiograceoio@gmail.com>
2019-03-20 23:42:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 10 LIMIT 1
+      WHERE grade = 10
       SQL
     DB[:conn].execute(sql).map do |row|
       student = self.new
